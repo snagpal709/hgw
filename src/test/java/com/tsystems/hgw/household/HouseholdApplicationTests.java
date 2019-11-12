97e1208733f53fdd6fea7729b6e6df74f5e6911b
Saransh Nagpal <saranshnagpal@Saransh-Nagpal-MacBook-Pro.local>
2019-11-12 14:26:10 +0530

    Initial commit

---
+++ src/test/java/com/tsystems/hgw/household/HouseholdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tsystems.hgw.household;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HouseholdApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
